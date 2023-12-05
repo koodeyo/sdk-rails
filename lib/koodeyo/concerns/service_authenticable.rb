79c1abf0b3e5d27d256f91533ef790d009c7b0a9
Paul Jeremiah Mugaya <paulmugaya@live.com>
2023-12-05 18:55:24 +0300

    service auth concern

---
+++ lib/koodeyo/concerns/service_authenticable.rb
@@ -0,0 +1,78 @@
+module Koodeyo
+  module Concerns
+    module ServiceAuthenticable
+      extend ActiveSupport::Concern
+
+      included do
+        attr_reader :current_service, :service_id, :service_secret, :service_access_token,
+        :service_accounts_api, :service_details
+      end
+
+      private
+
+      def authenticate_service!
+        # set service
+        set_service
+
+        # Skip
+        return if current_service.present?
+
+        @service_id = request.headers['Koodeyo-App-Id']
+        @service_secret = request.headers['Koodeyo-App-Secret']
+
+        if service_id.nil? || service_secret.nil?
+          return render_error("Missing Authorization headers")
+        end
+
+        # get access_token
+        @service_access_token = get_access_token
+
+        # get service details
+        @service_details = get_service_details
+
+        # Only allow official apps/services
+        return render_error("App not failed to authenticate") unless @service_details['is_official']
+
+        @current_service = save_service
+      end
+
+      def get_access_token
+        @service_accounts_api ||= Sdk::Accounts.new({
+          authorization: {
+            grant_type: 'client_credentials',
+            client_id: service_id,
+            client_secret: service_secret,
+            scope: 'read'
+          },
+          headers: {
+            "Skip-Devise-Doorkeeper": "true"
+          }
+        })
+
+        response = @service_accounts_api.get_access_token
+        return unless response.success?
+
+        response.to_json['access_token']
+      end
+
+      def get_service_details
+        response = service_accounts_api.find_app_by_uid(service_id)
+        return unless response.success?
+
+        service_accounts_api.find_app_by_uid(service_id).to_json
+      end
+
+      def render_error(message)
+        render json: { message: }, status: :unauthorized
+      end
+
+      def save_service
+        # save :service_id, :service_details
+      end
+
+      def set_service
+        # Try looking for the service from database
+      end
+    end
+  end
+end
